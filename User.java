5a20cfba0497204802ddbad6f49a9908cd53357c
rnatasha <nikki.rao@hotmail.com>
2016-05-14 12:52:10 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,4 +1,18 @@
 public class User
 {
-
+  private int myLives;
+  private double myX, myY;
+  
+  public User(){
+    myLives=3;
+  }
+  public void attack(Enemy e){
+    e.setLives(e.getLives()-1);
+  }
+  public void draw(Graphics myBuffer){
+    //not finished
+  }
+  public boolean isTouching(Enemy e){
+    //not finished
+  }
 }
