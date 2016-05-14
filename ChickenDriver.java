4c25cc995cc4e226aeac8760f96b358e1532aed2
Natasha R <rnatasha@users.noreply.github.com>
2016-05-14 15:50:03 -0400

    Update ChickenDriver.java

--- ChickenDriver.java
+++ ChickenDriver.java
@@ -2,5 +2,8 @@ public class ChickenDriver
 {
   public static void main(String[] args){
   //testing
+  //everything below is not real just testing parts of the classes
+  User u = new User();
+  Chicken c = new Chicken();
   }
 }
