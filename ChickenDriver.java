1bf3441cf30f7a12fa0d94361378632a0a79a31e
Natasha R <rnatasha@users.noreply.github.com>
2016-05-14 15:55:00 -0400

    Update ChickenDriver.java

--- ChickenDriver.java
+++ ChickenDriver.java
@@ -4,6 +4,18 @@ public class ChickenDriver
   //testing
   //everything below is not real just testing parts of the classes
   User u = new User();
-  Chicken c = new Chicken();
+  int level = 1;
+  Chicken[][] chickenPalooza = new Chicken[5][10];
+  double x=0;
+  double y=0;
+  //initializing chickens
+  for(int r= 0;r<chickenPalooza.length;r++){
+    for(int c=0;c<chickenPalooza[0].length;c++){
+      chickenPalooza[r][c]=new Chicken(level,x, y);
+      x+=10;
+    }
+    y+=10;
+  }
+  
   }
 }
