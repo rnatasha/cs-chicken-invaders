2368eb12fe05f094b61adc5ea1704400009d52f5
rnatasha <nikki.rao@hotmail.com>
2016-05-14 11:26:45 -0400

    Update Chicken.java

--- Chicken.java
+++ Chicken.java
@@ -1,4 +1,38 @@
 public class Chicken extends Enemy
 {
+  private int myLives, mySize;
+  private double myX, myY;
+  
+  public Chicken(double x, double y)
+  {
+    myLives=3;
+    mySize=20; //SOME DEFAULT SIZE FOR ALL THE SMALL CHICKENS?
+    myX=x;
+    myY=y;
+  }
+  public int getLives(){
+    return myLives;
+  }
+  public int getSize(){ //not sure if we'll be needing this
+    return mySize;
+  }
+  public double getX(){
+    return myX;
+  }
+  public double getY(){
+    return myY;
+  }
+  public void setLives(int x){ //not sure if this is necessary either, or if attack method will do
+    myLives=x;
+  }
+  public void setX(double x){
+    myX=x;
+  }
+  public void setY(double y){
+    myY=y;
+  }
+  public void attack(User u){
+    u.setLives(u.getLives()-1);
+  }
 
 }
