a26e33548924626e9ee23a8083710bfffdd794e2
Natasha R <rnatasha@users.noreply.github.com>
2016-05-16 23:46:14 -0400

    Update Egg.java

--- Egg.java
+++ Egg.java
@@ -1,10 +1,12 @@
 public class Egg extends Enemy
 {
+  private int myLives;
   private double myX, myY;
   
   public Egg(double x, double y){
     myX = x;
     myY = y;
+    myLives=1;
   }
   public double getX(){
     return myX;
@@ -12,15 +14,24 @@ public class Egg extends Enemy
   public double getY(){
     return myY;
   }
+  public int getLives(){
+      return myLives;
+  }
   public void setX(double x){
     myX=x;
   }
   public void setY(double y){
     myY=y;
   }
+  public void setLives(int n){
+    myLives=n;
+  }
+  public void attack(User u){
+    u.setLives(u.getLives()-1);
+  }
   public void release(double bottomEdge){
-    while(y!=bottomEdge){
-      y++;
+    while(myY!=bottomEdge){
+      myY++;
     }
   }
 }
