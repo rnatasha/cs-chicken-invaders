ff5fd5e069ce14cab9947aded86b2cf98d30284b
annen31 <2019anguyen@tjhsst.edu>
2016-05-17 08:02:00 -0400

    Update User.java

--- User.java
+++ User.java
@@ -33,11 +33,14 @@ public class User
   public void attack(Enemy e){
     e.setLives(e.getLives()-1);
   }
-  public void draw(Graphics myBuffer){
-    //not finished
+  public void draw(Graphics myBuffer, String filename){
+    ImageIcon pic = new ImageIcon(filename);
+    myBuffer.drawImage(pic.getImage(), 80, 80, 175, 150, null); //not the exact dimensions
   }
   public boolean isTouching(Enemy e){
-    //not finished
+    double d = distance(myX, myY, e.getX(), e.getY());
+        if(d <= b.getRadius() + pd.getRadius())
+         
     return true;
   }
   public void move(int x){//when used x= 1 or -1, for left or right movement along bottom of screen
