8f3a939ef022876508c2edc3ed70e9f1527415f7
Natasha R <rnatasha@users.noreply.github.com>
2016-05-16 23:45:49 -0400

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,5 +1,7 @@
 import javax.swing.ImageIcon;
 public abstract class Enemy
 {
-  public abstract void attack(int n){};
+  public abstract void attack(User u);
+  public abstract void setLives(int n);
+  public abstract int getLives();
 }
