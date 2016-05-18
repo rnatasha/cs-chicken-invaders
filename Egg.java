5bba0ec449a55852c2a2a20d96a44b79ea297230
Natasha R <rnatasha@users.noreply.github.com>
2016-05-18 18:53:13 -0400

    Update Egg.java

--- Egg.java
+++ Egg.java
@@ -11,8 +11,8 @@ public class Egg extends Enemy
   private int myLives, mySize;
   private double myX, myY;
   /************************************************************* 
-   * Constructs an Egg with initial coordinates specified by x and y
-   * and the number of lives specified by 1.
+   * Constructs an Egg with initial coordinates specified by x and y,
+   * lives set to 1, and size set to 20.
    **************************************************************/
   public Egg(double x, double y){
     myX = x;
@@ -21,14 +21,14 @@ public class Egg extends Enemy
     mySize=20;
   }
    /*************************************************************** 
-   * Returns the Egg's x coordinate
+   * Returns the Egg's x-coordinate
    * @return	 X
    **************************************************************/
   public double getX(){
     return myX;
   }
    /*************************************************************** 
-   * Returns the Egg's y coordinate
+   * Returns the Egg's y-coordinate
    * @return	 Y
    **************************************************************/
   public double getY(){
@@ -50,35 +50,35 @@ public class Egg extends Enemy
   }
    /***************************************************************
    * Sets the x coordinate
-   * @param x	 assigns x to myX or the Egg's x coordinate
+   * @param x	 assigns x to myX or the Egg's x-coordinate
    **************************************************************/
   public void setX(double x){
     myX=x;
   }
    /***************************************************************
    * Sets the y coordinate
-   * @param y	 assigns y to myY or the Egg's y coordinate
+   * @param y	 assigns y to myY or the Egg's y-coordinate
    **************************************************************/
   public void setY(double y){
     myY=y;
   }
    /***************************************************************
    * Sets the number of lives
-   * @param x	 assigns x to myLives
+   * @param n	 assigns n to myLives
    **************************************************************/
   public void setLives(int n){
     myLives=n;
   }
    /***************************************************************
    * Attacks the User passed by subtracting 1 life
-   * @param u	sets User's lives to current lives minus 1 
+   * @param u	sets User's lives to current lives minus 1. 
    **************************************************************/
   public void attack(User u){
     u.setLives(u.getLives()-1);
   }
    /***************************************************************
    * Increases the y coordiante of Egg object until it reaches the bottomEdge of the screen.
-   * @param bottomEdge	increases y-coordinate until equal to bottomEdge
+   * @param bottomEdge	increases Egg's y-coordinate until equal to bottomEdge
    **************************************************************/
   public void release(double bottomEdge){
     while(myY!=bottomEdge){
