89a84b3f8e4e4624930012f6f9f7980c645d6b72
Natasha R <rnatasha@users.noreply.github.com>
2016-05-18 13:51:39 -0400

    Create StartFrame.java

---
+++ StartFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+public class StartFrame{
+   public static void main(String[] args){
+      JFrame frame = new JFrame("Start Frame");
+      frame.setVisible(true);
+      frame.setLocation(100,100);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setContentPane(new StartPanel(frame));
+      frame.setSize(100,100);
+   }
+}
