f5b1d11a3a40ec44dafe0df4ed43aec5da953d78
Natasha R <rnatasha@users.noreply.github.com>
2016-05-18 14:04:01 -0400

    Update Rocket.java

--- Rocket.java
+++ Rocket.java
@@ -1,10 +1,26 @@
 public class Rocket
 {
   private double myX, myY;
-  private int mySize;
   
-   public void attack(Enemy e){
+  public Rocket(double x, double y){
+    myX=x;
+    myY=y;
+    
+  }
+   
+  public double getX(){
+    return myX;
+  }
+  public double getY(){
+    return myY;
+  }
+  public void setX(double x){
+    myX=x;
+  }
+  public void setY(double y){
+    myY=y;
+  }
+  public void attack(Enemy e){
     e.setLives(e.getLives()-1);
   }
-  
 }
