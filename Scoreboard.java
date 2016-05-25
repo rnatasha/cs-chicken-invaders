a747db2e3a7888db16b98d9048e842be19e86051
annen31 <2019anguyen@tjhsst.edu>
2016-05-25 14:15:37 -0400

    Update Scoreboard.java

--- Scoreboard.java
+++ Scoreboard.java
@@ -1,17 +1,27 @@
-   //Name______________________________ Date_____________
+
    import javax.swing.*;
    import java.awt.*; //testing to see if two people can work on it at once 
-   public class Scoreboard10 extends JPanel
+   public class Scoreboard extends JPanel
    {
-      private JLabel label1, label2, label3;
-      private int run, max;
+      private JLabel highscore, score1, score2, score3, score4, score5;
       public Scoreboard10()
       {
-         setLayout(new BoxLayout(1, 5));
+         setLayout(new BoxLayout(1, 3)); //five scores, "high score" label, and button to play again
+         add(new JLabel("HIGH SCORE", SwingConstants.CENTER));
+         highscore.setFont(new Font("Century Gothic", Font.BOLD, 60));
+         highscore.setHorizontalAlignment(SwingConstants.CENTER);
+         add(highscore);
          
-         run = max = 0;
-      
-         add(new JLabel("Run: ", SwingConstants.RIGHT));
+         JPanel subpanel = new JPanel();
+         panel.setLayout(new BoxLayout(1, 5));
+         add(panel);
+         
+         label = new JLabel("");
+         label.setFont(new Font("Serif", Font.BOLD, 30));
+         label.setHorizontalAlignment(SwingConstants.CENTER);
+         label.setForeground(Color.black);
+         add(label, BorderLayout.NORTH);
+         /*add(new JLabel("Run: ", SwingConstants.RIGHT));
          label1 = new JLabel(""+run);
          label1.setHorizontalAlignment(SwingConstants.LEFT);
          add(label1);
@@ -22,7 +32,7 @@
          add(new JLabel("Max: ", SwingConstants.RIGHT));
          label3 = new JLabel(""+max);
          label3.setHorizontalAlignment(SwingConstants.LEFT);
-         add(label3);
+         add(label3);*/
       }
       public void update(boolean arg) //if arg is true, the player guessed correctly
       {
