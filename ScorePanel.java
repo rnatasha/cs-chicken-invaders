8b992b76eab0ee982ae8d66a26b65b31a49bf444
annen31 <2019anguyen@tjhsst.edu>
2016-05-27 10:54:53 -0400

    Update ScorePanel.java

--- ScorePanel.java
+++ ScorePanel.java
@@ -3,12 +3,23 @@ import java.util.*;    //the Scanner class
 import javax.swing.JOptionPane;
 import java.util.Comparator;
 public class ScorePanel
-{
+{ //print scores & names into data.txt before running this
+  //order: name, score
   public static void main(String[] args) throws Exception
+  { 
+    User[] array = input("data.txt");
+  }
+  public static User[] input(String filename)
   {
-    Scanner infile = new Scanner(new File("data.txt"));
-    int numitems = infile.nextInt();
-    String[] array = new String[numitems];
-    for(int k = 0; k < numitems; k++)
+    Scanner infile = null;
+    try{
+      infile = new Scanner(new File(filename));
+    }
+    catch (FileNotFoundException e)
     {
-      
+      JOptionPane.showMessageDialog(null, "The file could not be found.");
+      System.exit(0);
+    }
+    }
+  }
+ 
