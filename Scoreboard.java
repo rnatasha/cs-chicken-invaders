ee69ae3df7f5d33b0e2b568ff054b518ce30e722
annen31 <2019anguyen@tjhsst.edu>
2016-06-03 13:42:59 -0400

    Update Scoreboard.java

--- Scoreboard.java
+++ Scoreboard.java
@@ -1,14 +1,32 @@
    import java.awt.Color;
+   import java.util.Comparator;
    import javax.swing.*;
    import java.awt.*; 
+   import java.io.*;      //the File class
+   import java.util.*;    //the Scanner class
+   import javax.swing.JOptionPane;
    public class Scoreboard extends JPanel
    {
       private JLabel highscore, score1, score2, score3, score4, score5;
       Image background;
+      User[] array;
+      Scanner infile;
+      
+      public static void main(String[] args)
+      {
+         array = input("data.txt");
+         sort(array);
+         output(array);
+      }
       public Scoreboard10()
       {
+         for(int k = 0; k < 5; k++)
+         {
+            array[k] = infile.next() + infile.nextInt();
+         }
+         
          background = Toolkit.getDefaultToolkit().createImage("//insert picture name");
-         setLayout(new BoxLayout(1, 3)); //five scores, "high score" label, and button to play again
+         setLayout(new BoxLayout(1, 3)); //five scores, "high score" label, and button to exit
          add(new JLabel("HIGH SCORE", SwingConstants.CENTER));
          highscore.setFont(new Font("Century Gothic", Font.BOLD, 60));
          highscore.setHorizontalAlignment(SwingConstants.CENTER);
@@ -18,7 +36,7 @@
          subpanel.setLayout(new BoxLayout(1, 5));
          add(subpanel);
          
-         score1 = new JLabel("");
+         score1 = new JLabel(array[0].getName+"\t"+array[0].getScore();
          score1.setFont(new Font("Serif", Font.BOLD, 30));
          score1.setForeground(new Color(179, 218, 255));
          score1.setHorizontalAlignment(SwingConstants.LEFT);
@@ -76,4 +94,69 @@
             System.out.exit(0);
          }
       }
-   }
+      
+        public static User[] input(String filename)
+        {
+          infile = null;
+          try{
+            infile = new Scanner(new File(filename));
+          }
+          catch (FileNotFoundException e)
+          {
+            JOptionPane.showMessageDialog(null, "The file could not be found.");
+            System.exit(0);
+          }
+          int numitems = infile.nextInt();
+          array = new User[numitems];
+          for(int k = 0; k < numitems; k++)
+            array[k] = new User(infile.next(), infile.nextInt());
+          infile.close();
+          return array;
+          }
+          
+      public static User[] sort(User[] array)  //need findMin method
+      {
+        int maxPos;
+        for(int k = 0; k < array.length; k++)
+        {
+          maxPos = findMax(array, array.length - k, c);
+          swap(array, maxPos, array.length-k-1);
+        }
+      }
+      
+      public static int findMin(User[] array, int upper)
+      {
+        int maxPos = 0;
+        for(int j = 1; j < upper; j++)
+        {
+          if(array[j].getScore() > array[maxPos].getScore())
+            maxPos = j;
+        }
+        return maxPos;
+        }
+      }
+      
+      public static void swap(User[] array, int a, int b)
+      {
+        User temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+      }
+      
+      public static void output(User[] array) throws Exception
+      {
+        PrintStream outfile = null;
+        try{
+          outfile = new PrintStream(new FileOutputStream("data.txt"));
+        }
+        catch(FileNotFoundException e)
+         {
+        JOptionPane.showMessageDialog(null, "The file could not be created.");
+        for(int k = 0; k < array.length; k++)
+          {
+            outfile.println(array[k].getName() + "\n" + array[k].getScore());
+          }
+       outfile.close();
+          }
+        }
+         }
