c1e0f4280cc187acf7455c292e3ed8c5b1959f52
annen31 <2019anguyen@tjhsst.edu>
2016-06-05 22:10:27 -0400

    Update StartPanel.java

--- StartPanel.java
+++ StartPanel.java
@@ -7,5 +7,42 @@ public class StartPanel extends JPanel
    JFrame newFrame;
    public StartPanel(JFrame frame){
       this.frame=frame;
+      
+      background = Toolkit.getDefaultToolkit().createImage("//insert picture name");
+      setLayout(new BorderLayout()); //five scores, "high score" label, and button to exit
+      add(new JLabel("HIGH SCORE", SwingConstants.CENTER));
+      highscore.setFont(new Font("Century Gothic", Font.BOLD, 60));
+      highscore.setHorizontalAlignment(SwingConstants.CENTER);
+      add(highscore, BorderLayout.NORTH);
+      
+      JButton start = new JButton("Start");
+      start.addActionListener(new StartListener());
+      
+      JButton help = new JButton("Help");
+      help.addActionListener(new HelpListener());
    }
+      public void paintComponent(Graphics g)
+   {
+      ImageIcon leftChicken = new ImageIcon("Chicken without background.png");
+      g.drawImage(leftChicken.getImage(), 80, 400, 100, 100, null);
+      
+      ImageIcon rightChicken = new ImageIcon("Chicken without background.png");
+      g.drawImage(rightChicken.getImage(), 320, 400, 100, 100, null);
+   }
+   
+   private class StartListener implements ActionListener
+      {
+         public void actionPerformed(ActionEvent e)
+         {
+            //start coding
+         }
+      }
+      
+   private class HelpListener implements ActionListener
+      {
+         public void actionPerformed(ActionEvent e)
+         {
+            //help coding
+         }
+      }
 }
