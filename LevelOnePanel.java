f94f1e18197fcdda3cdb19214dddc38e22e97783
annen31 <2019anguyen@tjhsst.edu>
2016-06-07 23:21:43 -0400

    Update LevelOnePanel.java

--- LevelOnePanel.java
+++ LevelOnePanel.java
@@ -5,68 +5,60 @@ import java.awt.image.*;
 
 public class LevelOnePanel extends JPanel
 {
-      private BufferedImage myImage;
-      private Graphics myBuffer;
-    String name;
-    ImageIcon avatar = new ImageIcon("bunny.png");
-    Chicken[] chickens = new Chicken[60];
-    Timer t;
-     User  player;
-     myImage =  new BufferedImage(FRAME, FRAME, BufferedImage.TYPE_INT_RGB);
-     myBuffer = myImage.getGraphics();
+   private Graphics buffer = image.getGraphics();
+   String name;
+   ImageIcon avatar = new ImageIcon("bunny.png");
+   Chicken[] chickens = new Chicken[60];
+   Timer t;
+   User player;
 
-    public LevelOnePanel(){
+   public LevelOnePanel(){
       // t = new Timer(5, new Listener());
         // t.start();
         // addMouseListener(new Mouse());
          
-         addKeyListener(new Key());
-         setFocusable(true);
-    }
-     public void paint (Graphics myBuffer) //okay honestly i have no idea if any of this is right
-    {
-        for(int y=15;y<=195;y+=90){
-      for(int x = 100; x<=1810; x+=90){
-        chickens[c]= new Chicken(1,x,y);
-        g.drawImage(chickens[c].getImageIcon().getImage(),chickens[c].getX(), chickens[c].getY(), 80,80,null);
-        c++;
-        chickens[I].update(myBuffer);
-      }
-    }
-    player.update(myBuffer);
-        
-    public void paintComponent(Graphics g)
-    {
-      ImageIcon background = new ImageIcon("background.png");
-      g.drawImage(background.getImage(), 0,0,2000,1000,null);
-     int c=0;
-     for(int y=15;y<=195;y+=90){
-      for(int x = 100; x<=1810; x+=90){
-      //we need to change the values for the coordinates, spacing is off
-        chickens[c]= new Chicken(1,x,y);
-        g.drawImage(chickens[c].getImageIcon().getImage(),chickens[c].getX(), chickens[c].getY(), 80,80,null);
-        c++;
-      }
-    }
-     player = new User(name, avatar);
-     g.drawImage(player.getImageIcon().getImage(),player.getX(), player.getY(),150,150,null);//write User get Image method.
-    }
-    
-    private class Key extends KeyAdapter{
-         public void keyPressed(KeyEvent e){
-            if(e.getKeyCode() == KeyEvent.VK_W)
-               player.setY(player.getY()-10);
-               if(e.getKeyCode() == KeyEvent.VK_Z)
-               player.setY(player.getY()+10);
-               if(e.getKeyCode() == KeyEvent.VK_A)
-               player.setX(player.getX()-10);
-               if(e.getKeyCode() == KeyEvent.VK_S)
-               player.setX(player.getX()-10);
-               repaint();
+      addKeyListener(new Key());
+      setFocusable(true);
+   }
+   public void paint(Graphics buffer) //c is not a variable defined here, needs fixing
+   {
+      for(int y=15;y<=195;y+=90){
+         for(int x = 100; x<=1810; x+=90){
+            chickens[c]= new Chicken(1,x,y);
+            g.drawImage(chickens[c].getImageIcon().getImage(),chickens[c].getX(), chickens[c].getY(), 80,80,null);
+            c++; //what is c referring to? is buffer increasing?
+            chickens[I].update(buffer);
          }
       }
-      
-   /* public ImageIcon updateLives(){
+      player.update(buffer); //need integer after here (how much moves)
+   }   
+   public void paintComponent(Graphics g, String name, ImageIcon avatar) {
+      ImageIcon background = new ImageIcon("background.png");
+      g.drawImage(background.getImage(), 0,0,2000,1000,null);
+      int c=0;
+      for(int y=15;y<=195;y+=90){
+         for(int x = 100; x<=1810; x+=90){
+            chickens[c]= new Chicken(1,x,y);
+            g.drawImage(chickens[c].getImageIcon().getImage(),chickens[c].getX(), chickens[c].getY(), 80,80,null);
+            c++;
+         }
+      }
+      player = new User(name, avatar);
+      g.drawImage(player.getImageIcon().getImage(),player.getX(), player.getY(),150,150,null);//write User get Image method.
+   }
     
-    }*/
+   private class Key extends KeyAdapter{
+      public void keyPressed(KeyEvent e){
+         if(e.getKeyCode() == KeyEvent.VK_W)
+            player.setY(player.getY()-10);
+         if(e.getKeyCode() == KeyEvent.VK_Z)
+            player.setY(player.getY()+10);
+         if(e.getKeyCode() == KeyEvent.VK_A)
+            player.setX(player.getX()-10);
+         if(e.getKeyCode() == KeyEvent.VK_S)
+            player.setX(player.getX()-10);
+         repaint();
+      }
+   }
 }
+
