34d0caa101495e85e4264f153b032d900dd66233
annen31 <2019anguyen@tjhsst.edu>
2016-06-07 22:27:24 -0400

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -4,7 +4,7 @@ public abstract class Enemy
   public abstract void attack(User u);
   public abstract void setLives(int n);
   public abstract int getLives();
-  public abstract double getX();
-  public abstract double getY();
+  public abstract int getX();
+  public abstract int getY();
   public abstract int getSize();
 }
